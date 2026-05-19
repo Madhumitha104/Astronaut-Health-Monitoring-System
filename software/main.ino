48baa2ff1bb641c32c10aa73f97aa3600f54bee8
Madhumitha104 <mitha8675091622@gmail.com>
2026-05-19 08:52:35 +0530

    Update main.ino

--- software/main.ino
+++ software/main.ino
@@ -1,2 +1,33 @@
 // Astronaut Health Monitoring System
-// ESP32 Main Code - Coming Soon
+// Hardware: ESP32 + MAX30102 + DS18B20
+// Author: Madhumitha R
+
+#include <Wire.h>
+#include <MAX30105.h>
+#include <DallasTemperature.h>
+
+MAX30105 particleSensor;
+OneWire oneWire(4);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  Serial.begin(115200);
+  sensors.begin();
+  particleSensor.begin();
+  Serial.println("System Started...");
+}
+
+void loop() {
+  // Heart Rate
+  long irValue = particleSensor.getIR();
+  Serial.print("IR Value: ");
+  Serial.println(irValue);
+
+  // Temperature
+  sensors.requestTemperatures();
+  float tempC = sensors.getTempCByIndex(0);
+  Serial.print("Temp: ");
+  Serial.println(tempC);
+
+  delay(1000);
+}
